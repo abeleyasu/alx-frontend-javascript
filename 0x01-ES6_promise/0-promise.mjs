3a7f36bc57d8443885e0c4ff26d9033a8635d8ae
abeleyasu <abeleyasu98@gmail.com>
2024-10-02 17:05:34 +0000

    task 0

---
+++ 0x01-ES6_promise/0-promise.mjs
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an async operation that resolves successfully
+    resolve('Success');
+  });
+}
