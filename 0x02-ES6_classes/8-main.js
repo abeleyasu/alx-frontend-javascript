3ca7e86297a09c048e93aecd71482df1043d26aa
abeleyasu <abeleyasu98@gmail.com>
2024-10-03 21:20:17 +0000

    task 7-11

---
+++ 0x02-ES6_classes/8-main.js
@@ -0,0 +1,5 @@
+import HolbertonClass from './8-hbtn_class.js';
+
+const hc = new HolbertonClass(12, "Mezzanine");
+console.log(Number(hc));    // Should output: 12
+console.log(String(hc));     // Should output: Mezzanine
