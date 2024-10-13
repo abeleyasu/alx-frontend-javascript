f672a28324d1c660a2bbfa477656285c4d10004e
abeleyasu <abeleyasu98@gmail.com>
2024-10-13 05:43:47 +0000

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,14 @@
-export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ];
-}
+// 0-get_list_students.js
+/**
+ * Function that returns a list of students.
+ * @returns {Array} Array of student objects with id, firstName, and location.
+ */
+const getListStudents = () => {
+    return [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ];
+};
+
+export default getListStudents;
